1e673897657040387162a61fdd2f075346a06b40
yashm333 <yashmarkendey2025@u.northwestern.edu>
2024-02-12 15:42:35 -0800

    yh

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+const header = document.querySelector("#header")
+const image = document.querySelector("#image")
+const credit = document.querySelector("#credit")
+
+
+
 document.querySelector(#chanterelle-button").addEventListener("click", function(){
-    alert ("You clicked a button")
-)}
+    header.innerHTML = "Chanterelle"
+    image.src = "chanterelle.jpg"
+    credit.innerHTML = "Neil Leclerc"
+})
+
+document.querySelector(#oyster-button").addEventListener("click", function(){
+    header.innerHTML = "Oyster"
+    image.src = "oyster.jpg"
+    credit.innerHTML = "Neil Leclerc"
+})
+
+document.querySelector(#chanterelle-button").addEventListener("click", function(){
+    header.innerHTML = "Chanterelle"
+    image.src = "chanterelle.jpg"
+    credit.innerHTML = "Neil Leclerc"
+})
+
+document.querySelector(#fly-agaric-button").addEventListener("click", function(){
+    header.innerHTML = "Fly Agaric"
+    image.src = "fly-agaric.jpg"
+    credit.innerHTML = "Neil Leclerc"
+})
